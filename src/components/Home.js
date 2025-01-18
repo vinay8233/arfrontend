50250c7b59bfe8e82cdc61ea0245775b99cbc943
vinay8233 <vinaypratap9660@gmail.com>
2025-01-18 17:19:29 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
   return (
     <>
 
-<div id="carouselExample" class="carousel slide">
+{/* <div id="carouselExample" class="carousel slide">
         <div className="carousel-inner">
           {product.map((item) => (
             <div className="carousel-item active">
@@ -45,7 +45,45 @@ const Home = () => {
           <span className="carousel-control-next-icon bg-dark" aria-hidden="true" />
           <span className="visually-hidden">Next</span>
         </button>
+      </div> */}
+
+<div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    {product.map((item, index) => (
+      <div
+        className={`carousel-item ${index === 0 ? "active" : ""}`}
+        key={item.id}
+      >
+        <img
+          className="d-block w-100"
+          src={`${process.env.REACT_APP_API_URL}/uploads/${item.img}`}
+          alt="Product"
+          style={{ maxHeight: "500px", objectFit: "cover" }}
+        />
       </div>
+    ))}
+  </div>
+
+  <button
+    className="carousel-control-prev"
+    type="button"
+    data-bs-target="#carouselExample"
+    data-bs-slide="prev"
+  >
+    <span className="carousel-control-prev-icon bg-dark rounded-circle" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+
+  <button
+    className="carousel-control-next"
+    type="button"
+    data-bs-target="#carouselExample"
+    data-bs-slide="next"
+  >
+    <span className="carousel-control-next-icon bg-dark rounded-circle" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
 
 
     </>
